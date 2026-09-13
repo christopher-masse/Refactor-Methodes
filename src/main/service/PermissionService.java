ff77e7c82d9abcb05495662a3ff79eeb68ddfc62
christopher-masse <157161948+christopher-masse@users.noreply.github.com>
2026-09-13 16:33:33 -0400

    Simplified and uniformized PermissionService

--- src/main/service/PermissionService.java
+++ src/main/service/PermissionService.java
@@ -5,20 +5,11 @@ import main.domain.Ship;
 
 public class PermissionService {
     public boolean canCarryHazardous(Ship ship) {
-        int permissions = ship.getPermissions();
-        if (permissions == Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == 15) return true;
-        return false;
+        return (ship.getPermissions() & Permissions.CARRY_HAZARDOUS) != 0;
     }
 
-    public boolean cannotCrossRestrictedSector(Ship ship) {
-        return (ship.getPermissions() & Permissions.CROSS_RESTRICTED_SECTOR) == 0;
+    public boolean canCrossRestrictedSector(Ship ship) {
+        return (ship.getPermissions() & Permissions.CROSS_RESTRICTED_SECTOR) != 0;
     }
 
     public void grantHazardousPermission(Ship ship) {
