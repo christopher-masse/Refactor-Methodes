e356adbba2cef881ee540c497df29194546ed82f
christopher-masse <157161948+christopher-masse@users.noreply.github.com>
2026-09-13 16:36:39 -0400

    Create and handle Risk Enum

--- src/main/service/RiskService.java
+++ src/main/service/RiskService.java
@@ -2,16 +2,17 @@ package main.service;
 
 import main.domain.Cargo;
 import main.domain.Shipment;
+import main.enums.Risk;
 import main.enums.SecurityLevel;
 
 public class RiskService {
-    public String evaluate(Shipment shipment) {
+    public Risk evaluate(Shipment shipment) {
         if ((shipment.getDestination().getSecurityLevel().isHigherThan(SecurityLevel.HIGH)
                 && !shipment.getOrigin().getSector().equals(shipment.getDestination().getSector()))
                 || (shipment.getCargo().stream().anyMatch(Cargo::isHazardous)
                 && shipment.getCargo().stream().mapToDouble(Cargo::getDeclaredValue).sum() > 50000)) {
-            return "CRITICAL";
+            return Risk.CRITICAL;
         }
-        return "NORMAL";
+        return Risk.NORMAL;
     }
 }
