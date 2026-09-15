8ab738aa8071be08756de5d50237677e82ded4ea
christopher-masse <157161948+christopher-masse@users.noreply.github.com>
2026-09-15 10:13:24 -0400

    Refactor getRewardPoints

--- src/main/service/AccountService.java
+++ src/main/service/AccountService.java
@@ -4,17 +4,20 @@ import main.domain.Customer;
 import main.exception.InsufficientFundsException;
 
 public class AccountService {
+    private final int LOYALTY_YEARS_REQUIRED = 5;
+    private final int REWARD_LOYALTY = 500;
+    private final int REWARD_REGULAR = 100;
+    
     public void withdraw(Customer customer, double amount) throws InsufficientFundsException {
         if (customer.getAccount().getAccountBalance() < amount) {
             throw new InsufficientFundsException("Fonds insuffisant");
         }
         customer.getAccount().setAccountBalance(customer.getAccount().getAccountBalance() - amount);
-
-    public boolean hasMoreThanFiveYears(Customer customer) {
-        return customer.getLoyaltyYears() > 5;
     }
 
     public int getRewardPoints(Customer customer) {
-        return hasMoreThanFiveYears(customer) ? 500 : 100;
+        return customer.getLoyaltyYears() > LOYALTY_YEARS_REQUIRED
+                ? REWARD_LOYALTY
+                : REWARD_REGULAR;
     }
 }
