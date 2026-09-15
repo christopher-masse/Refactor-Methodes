f585433e824f2774c012e5fd750c1d35de7f28f9
christopher-masse <157161948+christopher-masse@users.noreply.github.com>
2026-09-15 10:19:26 -0400

    Refactor canCarryHazardous + removed dead code

--- src/main/service/PermissionService.java
+++ src/main/service/PermissionService.java
@@ -6,26 +6,7 @@ import main.domain.Ship;
 public class PermissionService {
     public boolean canCarryHazardous(Ship ship) {
         int permissions = ship.getRegistration().getPermissions();
-        if (permissions == Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS) return true;
-        if (permissions == Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == Permissions.VIEW_MANIFEST + Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == Permissions.EDIT_MANIFEST + Permissions.CARRY_HAZARDOUS + Permissions.CROSS_RESTRICTED_SECTOR) return true;
-        if (permissions == 15) return true;
-        return false;
+        return (permissions & Permissions.CARRY_HAZARDOUS) != 0;
     }
 
-    public boolean cannotCrossRestrictedSector(Ship ship) {
-        return (ship.getRegistration().getPermissions() & Permissions.CROSS_RESTRICTED_SECTOR) == 0;
-    }
-
-    public void grantHazardousPermission(Ship ship) {
-        ship.getRegistration().setPermissions(ship.getRegistration().getPermissions() | Permissions.CARRY_HAZARDOUS);
-    }
-
-    public void removeEditPermission(Ship ship) {
-        ship.getRegistration().setPermissions(ship.getRegistration().getPermissions() & ~Permissions.EDIT_MANIFEST);
-    }
 }
